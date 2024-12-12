638a287b2a764f86b9247efb05deca53101d9abe
GGeunit <kie_love@naver.com>
2024-12-12 15:56:39 +0900

    환경설정

--- src/test/java/com/example/demo/TaskApplicationTests.java
+++ src/test/java/com/example/demo/TaskApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Task3ApplicationTests {
+class TaskApplicationTests {
 
 	@Test
 	void contextLoads() {
